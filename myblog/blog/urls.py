a3e8e889f485abe51369ebeeab42eae42b38145b
David <d11218700@gmail.com>
2026-08-03 18:31:20 +0600

    урок 05, добавлены комментарии, сверстал шаблон для детального просмотра статей

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.PostView.as_view())]
+urlpatterns = [
+    path("", views.PostView.as_view()),
+    path("<int:pk>/", views.PostDetail.as_view()),
+]
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -122,3 +122,5 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
