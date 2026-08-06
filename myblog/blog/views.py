b272cd27de64f6f390c263601f1fdd17f3bc00f6
David <d11218700@gmail.com>
2026-08-06 19:32:52 +0600

    Добавлена функция лайков/дизлайков и немного стилей

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
-from .models import Post
+from .models import Post, Likes
 from .form import CommentsForm
 
 
@@ -31,3 +31,41 @@ class AddComments(View):
             form.save()
 
         return redirect(f"/{pk}")
+
+
+def get_client_ip(request):
+    x_forward_for = request.META.get("HTTP_FORWARDED_FOR")
+
+    if x_forward_for:
+        ip = x_forward_for.split(",")[0]
+    else:
+        ip = request.META.get("REMOTE_ADDR")
+
+    return ip
+
+
+class AddLike(View):
+
+    def get(self, request, pk):
+        ip_client = get_client_ip(request)
+        try:
+            Likes.objects.get(ip=ip_client, pos_id=pk)
+
+        except:
+            new_like = Likes()
+            new_like.ip = ip_client
+            new_like.pos_id = int(pk)
+            new_like.save()
+
+        return redirect(f"/{pk}")
+
+
+class DelLike(View):
+    def get(self, request, pk):
+        ip_client = get_client_ip(request)
+        try:
+            lik = Likes.objects.get(ip=ip_client)
+            lik.delete()
+            return redirect(f"/{pk}")
+        except:
+            return redirect(f"/{pk}")
